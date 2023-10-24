7a328dd6e1462010b2685924382cb7d006254144
Quinn Davis <qdavis6106@stu.d214.org>
2023-10-24 11:01:55 -0500

    Firebase is setup

--- WEIGHT MANAGEMENT/WEIGHT_MANAGEMENTApp.swift
+++ WEIGHT MANAGEMENT/WEIGHT_MANAGEMENTApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
-struct WEIGHT_MANAGEMENTApp: App {
+struct YourApp: App {
+    //Register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
