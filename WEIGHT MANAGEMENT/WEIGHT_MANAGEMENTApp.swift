92f73850c90f89c97ffa00688bc70d7fb315d034
Tirth Patel <Tirthp0112@gmail.com>
2023-10-24 10:19:24 -0500

    Initial Commit

---
+++ WEIGHT MANAGEMENT/WEIGHT_MANAGEMENTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WEIGHT_MANAGEMENTApp.swift
+//  WEIGHT MANAGEMENT
+//
+//  Created by Tirth D. Patel on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WEIGHT_MANAGEMENTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
